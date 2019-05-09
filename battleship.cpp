376e321d5472b945dae51a0710bc4d2409050683
StanislavSemashko <stanislavsemashko5@gmail.com>
2019-05-09 11:36:28 +0200

    6 Tests passing

--- battleship.cpp
+++ battleship.cpp
@@ -13,3 +13,91 @@
 #include <stdio.h>
 
 #include "battleship.h"
+
+CellContent my_cell_content [FIELDSIZE][FIELDSIZE];
+CellContent other_cell_content [FIELDSIZE][FIELDSIZE];
+CellContent guess_cell_content [FIELDSIZE][FIELDSIZE];
+
+void load_game()
+{
+
+  FILE* fd_my = fopen("battleship.my", "r");
+  FILE* fd_op = fopen("battleship.op", "r");
+
+  fread(my_cell_content,sizeof(CellContent),100,fd_my);
+  fread(other_cell_content,sizeof(CellContent),100,fd_op);
+
+for (int i = 0; i < FIELDSIZE; i++)
+{
+  for (int j = 0; j < FIELDSIZE; j++)
+  {
+    guess_cell_content[i][j] = Unknown;
+  }
+}
+
+}
+
+/**
+*** @param row The row where we get the shot.
+*** @param col The col where we get the shot.
+*** @return The value of cell indicated by row and column of the own field (filled
+*** with the values of battleship.my). If row/col point outside the field the
+*** value OutOfRange is returned.
+*/
+CellContent get_shot(int row, int col)
+{
+  if (row >= FIELDSIZE || col >= FIELDSIZE || col < 0 || row < 0)
+  {
+    return OutOfRange;
+  }
+  return my_cell_content[row][col];
+}
+
+/**
+*** Sets the row and column of the my guesses table to the value held in the
+*** opponents field. If this is a boat all surrounding cells are marked as water.
+*** @param row The row where we place the shot
+*** @param col The columnt where we place the shot.
+*** @return True if target field is valid, i.e., inside the field, False otherwise.
+*/
+bool shoot(int row, int col)
+{
+  if (row >= FIELDSIZE || col >= FIELDSIZE || col < 0 || row < 0)
+  {
+    return false;
+  }
+  if (other_cell_content[row][col] == Boat)
+  {
+    guess_cell_content[row+1][col] = Water;
+    guess_cell_content[row-1][col] = Water;
+
+    guess_cell_content[row][col+1] = Water;
+    guess_cell_content[row][col-1] = Water;
+
+    guess_cell_content[row+1][col-1] = Water;
+    guess_cell_content[row-1][col+1] = Water;
+
+    guess_cell_content[row+1][col+1] = Water;
+    guess_cell_content[row-1][col-1] = Water;
+
+    guess_cell_content[row][col] = other_cell_content[row][col];
+    return true;
+  }
+  guess_cell_content[row][col] = other_cell_content[row][col];
+  return true;
+}
+
+/**
+*** @param row The row where we want to get our own guess.
+*** @param col The column where we want to get our own guess.
+*** @return The value of the table with my guesses at row and column. If row/col
+*** is outside the field OutOfRange is returned.
+*/
+CellContent get_my_guess(int row, int col)
+{
+  if (row >= FIELDSIZE || col >= FIELDSIZE || col < 0 || row < 0)
+  {
+    return OutOfRange;
+  }
+  return guess_cell_content[row][col];
+}
